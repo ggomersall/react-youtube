fdc5c6ad0503af1b20854204b85b9d9a7a57c270
Gareth <ggomersall@gmail.com>
2017-01-27 14:44:48 +0000

    changed index.js from functional to class based componetn

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,7 +6,7 @@ class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { term: '' };
+        this.state = { term: '' }; // Sets our state to be blank text initially
     }
 
     render() {
--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-//  this is a functional component
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDXP4oTTT1GCoDBHWSB-HDP_c-nUiMu0Fs';
 
-// Create new component and this component should produce
-// some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+// downwards flow data
+// is the most parent component that fetches data
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 // take this component generated html and put it
