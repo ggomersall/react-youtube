b2f01010c2c756a3c2fa2fae68b07284428bae71
Gareth <ggomersall@gmail.com>
2017-01-27 15:00:16 +0000

    added parent compopnent of VideLiost using props of videos object from index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from  './components/video_list';
 
 const API_KEY = 'AIzaSyDXP4oTTT1GCoDBHWSB-HDP_c-nUiMu0Fs';
 
@@ -13,8 +14,9 @@ class App extends Component {
 
         this.state = { videos: [] };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-            this.setState({ videos: data });
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos }); 
+            // above is the same as writing this.setState({ videos: videos }); - but in ES6
         });
 
     }
@@ -23,6 +25,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
